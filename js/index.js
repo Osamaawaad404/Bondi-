04bacd8748e47760f8ce8e61a5f2b78b7a14608e
Osama shabam <osamaawaad02@gmail.com>
2024-08-02 12:48:46 +0300

    Bondi

---
+++ js/index.js
@@ -0,0 +1,34 @@
+import { db } from './database.js';
+import { Suspense } from 'react';
+
+async function ConferencePage({ slug }) {
+  const conf = await db.Confs.find({ slug });
+  return (
+    <ConferenceLayout conf={conf}>
+      <Suspense fallback={<TalksLoading />}>
+        <Talks confId={conf.id} />
+      </Suspense>
+    </ConferenceLayout>
+  );
+}
+
+
+
+
+
+
+import { Autocomplete, Input, Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Input, Ripple });
+
+const basicAutocomplete = document.querySelector('#search-autocomplete');
+const data = ['One', 'Two', 'Three', 'Four', 'Five'];
+const dataFilter = (value) => {
+  return data.filter((item) => {
+    return item.toLowerCase().startsWith(value.toLowerCase());
+  });
+};
+
+new Autocomplete(basicAutocomplete, {
+  filter: dataFilter
+});
